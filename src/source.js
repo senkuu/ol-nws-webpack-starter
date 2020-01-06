759412a0c986927e94c7baa8b86810952fb4676a
Enzo <dorosario.enzo@protonmail.com>
2020-01-06 11:50:09 +0100

    show text

--- src/source.js
+++ src/source.js
@@ -9,25 +9,13 @@ import {
 } from "ol/interaction";
 import TileLayer from "ol/layer/Tile";
 import { OSM, Vector as VectorSource } from "ol/source";
-import { Circle as CircleStyle, Stroke, Style, Fill } from "ol/style";
+import { Circle as CircleStyle, Stroke, Style, Fill, Text } from "ol/style";
 import { transform } from "ol/proj";
 import VectorLayer from "ol/layer/Vector";
 
 // coordonnées récupérées depuis https://www.latlong.net/convert-address-to-lat-long.html
 const nws = transform([1.06653, 49.42847], "EPSG:4326", "EPSG:3857");
 const copeaux = transform([1.06539, 49.42251], "EPSG:4326", "EPSG:3857");
-
-const image = new CircleStyle({
-  radius: 5,
-  fill: new Fill({ color: "purple" }),
-  stroke: new Stroke({ color: "purple", width: 1 })
-});
-
-const styles = {
-  Point: new Style({ image: image })
-};
-
-const styleFunction = feature => styles[feature.getGeometry().getType()];
 
 const geojsonObject = {
   type: "FeatureCollection",
@@ -40,6 +28,9 @@ const geojsonObject = {
   features: [
     {
       type: "Feature",
+      properties: {
+        name: "Normandie Web School"
+      },
       geometry: {
         type: "Point",
         coordinates: nws
@@ -47,21 +38,47 @@ const geojsonObject = {
     },
     {
       type: "Feature",
+      properties: {
+        name: "Copeaux"
+      },
       geometry: {
         type: "Point",
+        name: "copeaux",
         coordinates: copeaux
       }
     }
   ]
 };
 
-const vectorSource = new VectorSource({
-  features: new GeoJSON().readFeatures(geojsonObject)
-});
+const getText = (feature, resolution) => {
+  const text = feature.get("name");
+  return text;
+};
 
-const vectorLayer = new VectorLayer({
-  source: vectorSource,
-  style: styleFunction
+const createTextStyle = (feature, resolution) =>
+  new Text({
+    textAlign: "center",
+    text: getText(feature, resolution),
+    placement: "POINT",
+    font: "bold 18",
+    fill: new Fill({ color: "black" })
+  });
+
+const pointStyleFunction = (feature, resolution) =>
+  new Style({
+    image: new CircleStyle({
+      radius: 8,
+      fill: new Fill({ color: "purple" }),
+      stroke: new Stroke({ color: "purple", width: 1 })
+    }),
+    text: createTextStyle(feature, resolution)
+  });
+
+const vectorPoints = new VectorLayer({
+  source: new VectorSource({
+    features: new GeoJSON().readFeatures(geojsonObject)
+  }),
+  style: pointStyleFunction
 });
 
 var map = new Map({
@@ -70,7 +87,7 @@ var map = new Map({
     new TileLayer({
       source: new OSM()
     }),
-    vectorLayer
+    vectorPoints
   ],
   target: "carteNWS",
   view: new View({
